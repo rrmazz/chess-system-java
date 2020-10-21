7f3525d2d2edc38eec4b73ec2a2c121625934c89
rogrico@hotmail.com <rogrico@hotmail.com>
2020-10-21 09:18:05 -0300

    Chess layer and priting the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,14 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
-		System.out.println();
+		ChessMatch chess = new ChessMatch();
+		UI.printBoard(chess.getPieces());
 		
 
 	}
